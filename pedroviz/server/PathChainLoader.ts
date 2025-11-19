b8434cbba05ec5b7fde942796d685b7f23c544e1
Kevin Frei <kevinfrei@hotmail.com>
2025-11-19 14:33:00 -0800

    Successfully extracting public static double fields

--- pedroviz/server/PathChainLoader.ts
+++ pedroviz/server/PathChainLoader.ts
@@ -4,12 +4,12 @@ import {
   FieldDeclarationCtx,
   MethodBodyCtx,
   parse,
+  UnannTypeCtx,
 } from 'java-parser';
-import { NamedValue, PathChainFile } from './types';
+import { AnonymousValue, NamedValue, PathChainFile } from './types';
 import { promises as fsp } from 'node:fs';
-
+import { isDefined, isString } from '@freik/typechk';
 class PathChainLoader extends BaseJavaCstVisitorWithDefaults {
-  filename: string = '';
   content: string = '';
   parsed: ReturnType<typeof parse> | null = null;
   info: PathChainFile = {
@@ -28,7 +28,7 @@ class PathChainLoader extends BaseJavaCstVisitorWithDefaults {
 
   async loadFile(filename: string): Promise<string | true> {
     // Read the contents fo the file and parse it:
-    this.filename = filename;
+    this.info.name = filename;
     try {
       const content = await fsp.readFile(filename, 'utf-8');
       return this.parseContent(content);
@@ -49,7 +49,7 @@ class PathChainLoader extends BaseJavaCstVisitorWithDefaults {
     try {
       this.visit(this.parsed);
     } catch (e) {
-      return `Could not visit parsed CST for file: ${this.filename} - ${e}`;
+      return `Could not visit parsed CST for file: ${this.info.name} - ${e}`;
     }
     return true;
   }
@@ -61,6 +61,11 @@ class PathChainLoader extends BaseJavaCstVisitorWithDefaults {
 
   fieldDeclaration(ctx: FieldDeclarationCtx) {
     // console.log('fieldDeclaration ctx', ctx);
+    // We're looking for public static double/int name = value;
+    const maybeNamedValue = tryMatchingNamedValues(ctx);
+    if (isDefined(maybeNamedValue)) {
+      this.info.values.push(maybeNamedValue);
+    }
     return super.fieldDeclaration(ctx);
   }
 
@@ -70,18 +75,137 @@ class PathChainLoader extends BaseJavaCstVisitorWithDefaults {
   }
 }
 
+// This matches the 'public static int/double name = value;' pattern
+function tryMatchingNamedValues(
+  ctx: FieldDeclarationCtx,
+): NamedValue | undefined {
+  if (!ctx.fieldModifier || ctx.fieldModifier.length !== 2) {
+    return;
+  }
+  if (
+    !ctx.fieldModifier.every(
+      (mod) => mod.children.Public || mod.children.Static,
+    )
+  ) {
+    return;
+  }
+  if (ctx.unannType.length !== 1) {
+    return;
+  }
+  const theType: UnannTypeCtx = ctx.unannType[0].children;
+  if (!theType.unannPrimitiveTypeWithOptionalDimsSuffix) {
+    return;
+  }
+  const primTypes = theType.unannPrimitiveTypeWithOptionalDimsSuffix;
+  if (
+    primTypes.length !== 1 ||
+    isDefined(primTypes[0].children.dims) ||
+    primTypes[0].children.unannPrimitiveType.length !== 1
+  ) {
+    return;
+  }
+  const primType = primTypes[0].children.unannPrimitiveType;
+  if (
+    primType.length !== 1 ||
+    !primType[0].children.numericType ||
+    primType[0].children.numericType.length !== 1
+  ) {
+    return;
+  }
+  const value: AnonymousValue = { type: 'double', value: 0 };
+  const children = primType[0].children.numericType![0].children;
+  if (children.floatingPointType) {
+    if (children.floatingPointType[0].children.Float) {
+      return;
+    }
+  } else if (children.integralType) {
+    if (!children.integralType[0].children.Int) {
+      return;
+    }
+    value.type = 'int';
+  }
+  // Okay, found the type. Need the name and the initialized value.
+  if (ctx.variableDeclaratorList.length !== 1) {
+    return;
+  }
+  const varDecl = ctx.variableDeclaratorList[0].children.variableDeclarator;
+  if (varDecl.length !== 1) {
+    return;
+  }
+  const varDeclCtx = varDecl[0].children;
+  if (
+    !varDeclCtx.variableDeclaratorId ||
+    varDeclCtx.variableDeclaratorId.length !== 1 ||
+    !varDeclCtx.variableDeclaratorId[0].children.Identifier ||
+    varDeclCtx.variableDeclaratorId[0].children.Identifier.length !== 1 ||
+    !varDeclCtx.variableInitializer ||
+    varDeclCtx.variableInitializer.length !== 1
+  ) {
+    return;
+  }
+  const name = varDeclCtx.variableDeclaratorId[0].children.Identifier[0].image;
+  const initializer = varDeclCtx.variableInitializer[0].children.expression;
+  if (
+    !initializer ||
+    initializer.length !== 1 ||
+    !initializer[0].children.conditionalExpression ||
+    initializer[0].children.conditionalExpression.length !== 1
+  ) {
+    return;
+  }
+  const expr =
+    initializer[0].children.conditionalExpression[0].children.binaryExpression;
+  if (
+    !expr ||
+    expr.length !== 1 ||
+    expr[0].children.unaryExpression.length !== 1 ||
+    expr[0].children.unaryExpression[0].children.primary.length !== 1 ||
+    !expr[0].children.unaryExpression[0].children.primary[0].children
+      .primaryPrefix ||
+    expr[0].children.unaryExpression[0].children.primary[0].children
+      .primaryPrefix.length !== 1 ||
+    !expr[0].children.unaryExpression[0].children.primary[0].children
+      .primaryPrefix[0].children.literal ||
+    expr[0].children.unaryExpression[0].children.primary[0].children
+      .primaryPrefix[0].children.literal.length !== 1
+  ) {
+    return;
+  }
+  const lit =
+    expr[0].children.unaryExpression[0].children.primary[0].children
+      .primaryPrefix[0].children.literal;
+  if (value.type === 'int') {
+    if (
+      !lit[0].children.integerLiteral ||
+      !lit[0].children.integerLiteral[0].children.DecimalLiteral
+    ) {
+      return;
+    }
+
+    value.value = parseInt(
+      lit[0].children.integerLiteral[0].children.DecimalLiteral[0].image,
+    );
+  } else {
+    if (
+      !lit[0].children.floatingPointLiteral ||
+      !lit[0].children.floatingPointLiteral[0].children.FloatLiteral
+    ) {
+      return;
+    }
+    value.value = parseFloat(
+      lit[0].children.floatingPointLiteral[0].children.FloatLiteral[0].image,
+    );
+  }
+  return { name, value };
+}
+
 export async function MakePathChainFile(
   filename: string,
-): Promise<PathChainFile> {
+): Promise<PathChainFile | string> {
   const loader = new PathChainLoader();
   const res = await loader.loadFile(filename);
-  console.log('res', res);
-  return {
-    name: filename,
-    values: [], // NamedValue[];
-    poses: [], // NamedPose[];
-    beziers: [], // Bezier[];
-    pathChains: [], // PathChain[];
-    // heading?: HeadingType;
-  };
+  if (isString(res)) {
+    return res;
+  }
+  return loader.info;
 }
--- pedroviz/server/loadpath.ts
+++ pedroviz/server/loadpath.ts
@@ -1,18 +1,22 @@
 import { PathChainFile } from './types';
 import { getProjectFilePath } from './utility';
 import { MakePathChainFile } from './PathChainLoader';
+import { isString } from '@freik/typechk';
 
 export async function LoadPath(
   team: string,
   filename: string,
 ): Promise<Response> {
   const filePath = getProjectFilePath(team, filename);
-  const paths: PathChainFile = await loadPathChainsFromFile(filePath);
+  const paths = await loadPathChainsFromFile(filePath);
+  if (isString(paths)) {
+    return Response.json({ team, error: paths });
+  }
   return Response.json({ team, paths });
 }
 
 export async function loadPathChainsFromFile(
   filePath: string,
-): Promise<PathChainFile> {
-  return await MakePathChainFile(filePath);
+): Promise<PathChainFile | string> {
+  return MakePathChainFile(filePath);
 }
