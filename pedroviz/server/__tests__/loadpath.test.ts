2c5c411eced4e5740e5df70f8da6b69796a17cfd
Kevin Frei <kevinfrei@hotmail.com>
2025-11-19 20:17:12 -0800

    Added a failing test for Poses

--- pedroviz/server/__tests__/loadpath.test.ts
+++ pedroviz/server/__tests__/loadpath.test.ts
@@ -31,7 +31,42 @@ test('loadPathChainsFromFile loads paths correctly', async () => {
   if (isString(paths)) return;
   // This currently failing, as I haven't implemented the parsing yet.
   expect(paths.values.length).toBe(3);
-  expect(paths.values[0].name).toBe('org');
-  expect(paths.values[1].name).toBe('dist');
-  expect(paths.values[2].name).toBe('one80');
+  expect(paths.values[0]).toEqual({
+    name: 'org',
+    value: { type: 'double', value: 72.0 },
+  });
+  expect(paths.values[1]).toEqual({
+    name: 'step',
+    value: { type: 'int', value: 80 },
+  });
+  expect(paths.values[2]).toEqual({
+    name: 'one80',
+    value: { type: 'double', value: 3.1416 },
+  });
+
+  expect(paths.poses.length).toBe(5);
+  expect(paths.poses[0]).toEqual({
+    name: 'start',
+    pose: { x: 'org', y: 'org', heading: { type: 'int', value: 0 } },
+  });
+  expect(paths.poses[1]).toEqual({
+    name: 'step1',
+    pose: { x: 'step', y: 'org', heading: { type: 'double', value: 1.5708 } },
+  });
+  expect(paths.poses[2]).toEqual({
+    name: 'step2',
+    pose: { x: 'step', y: 'step', heading: 'one80' },
+  });
+  expect(paths.poses[3]).toEqual({
+    name: 'step3',
+    pose: { x: 'org', y: 'step', heading: { type: 'double', value: -0.7854 } },
+  });
+  expect(paths.poses[4]).toEqual({
+    name: 'step4',
+    pose: {
+      x: { type: 'double', value: 72.0 },
+      y: { type: 'int', value: 72 },
+      heading: { type: 'double', value: 0.5236 },
+    },
+  });
 });
