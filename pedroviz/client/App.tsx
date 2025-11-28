3e2752430773633cf1f25258bf23c0ed387b401a
Kevin Frei <kevinfrei@hotmail.com>
2025-11-28 08:20:45 -0800

    Made dark mode setting (not fully wired up yet)

--- pedroviz/client/App.tsx
+++ pedroviz/client/App.tsx
@@ -3,6 +3,7 @@ import { useRef, useEffect, ReactElement, useState } from 'react';
 import { getStore } from './state/Storage';
 import { PathSelector } from './PathSelector';
 import {
+  Checkbox,
   FluentProvider,
   webDarkTheme,
   webLightTheme,
@@ -15,8 +16,18 @@ import './index.css';
 
 export function App(): ReactElement {
   const store = getStore();
+  /*
+    For reference: To invert the brightness, but not the hue, of an image:
+    R = 1 - (G + B) / 2
+    G = 1 - (R + B) / 2
+    B = 1 - (R + G) / 2
+
+    And here's a gist that shows how to manually render an image:
+    https://gist.github.com/paulirish/373253
+  */
   const [theTheme, setTheme] = useState<'dark' | 'light'>('light');
   const theme = theTheme === 'dark' ? webDarkTheme : webLightTheme;
+
   /*
   const sidebarRef = useRef(null);
   const dragHandleRef = useRef(null);
@@ -69,7 +80,13 @@ export function App(): ReactElement {
             </div>
             <div className="header-center">Vote4Pedro</div>
             <div className="header-right">
-              <p>settings?</p>
+              <Checkbox
+                checked={theTheme === 'dark'}
+                onChange={(_, data) =>
+                  setTheme(data.checked ? 'dark' : 'light')
+                }
+                label="Dark Mode"
+              />
             </div>
           </div>
           <div
