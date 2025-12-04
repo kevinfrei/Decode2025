4d7400aca693f105b2b6fcb644cb001b52f383b7
Kevin Frei <kevinfrei@hotmail.com>
2025-12-04 13:51:19 -0800

    Got a some testing in place for the evaluation code

--- pedroviz/client/__tests__/AutoSelector.test.tsx
+++ pedroviz/client/__tests__/AutoSelector.test.tsx
@@ -7,6 +7,9 @@ import { expect, mock, test } from 'bun:test';
 import { AutoSelector } from '../ui-tools/AutoSelector';
 import './jest-dom-types-fix';
 
+// I'm *really* new to all this UI testing. These are all *terrible* tests,
+// but they're a start.
+
 test('AutoDisablingSelector tests: one item', async () => {
   const setSel = mock((val: string) => {});
   render(
@@ -19,8 +22,8 @@ test('AutoDisablingSelector tests: one item', async () => {
       />
     </FluentProvider>,
   );
-  const item = screen.getByRole('button');
-  expect(item).toBeEnabled();
+  const item = screen.getAllByRole('button');
+  expect(item[0]).toBeEnabled();
   await waitFor(() => expect(setSel).toBeCalledWith('1'));
 });
 
@@ -36,8 +39,8 @@ test('AutoDisablingSelector tests: two items', async () => {
       />
     </FluentProvider>,
   );
-  const item = screen.getByRole('button');
-  expect(item).toBeEnabled();
+  const item = screen.getAllByRole('button');
+  expect(item[0]).toBeEnabled();
   await waitFor(() => expect(setSel).toBeCalledTimes(0));
 });
 
@@ -57,8 +60,11 @@ test('AutoDisablingSelector tests: two items, default', async () => {
       />
     </FluentProvider>,
   );
-  const item = screen.getByRole('button');
-  expect(item).toBeEnabled();
+  const item = screen.getAllByRole('button');
+  expect(item.length).toBe(3);
+  expect(item[0]).toBeEnabled();
+  expect(item[1]).toBeEnabled();
+  expect(item[2]).toBeEnabled();
   expect(selItem).toEqual('');
   await waitFor(() => expect(setSel).toBeCalledTimes(0));
   expect(selItem).toEqual('2');
@@ -72,7 +78,11 @@ test('AutoDisablingSelector tests: no items', async () => {
       <AutoSelector prompt="Test" items={[]} selected="" setSelected={setSel} />
     </FluentProvider>,
   );
-  const item = screen.getByRole('button');
-  expect(item).toBeDisabled();
+  const items = screen.getAllByRole('button');
+  expect(items.length).toBe(4);
+  expect(items[0]).toBeEnabled();
+  expect(items[1]).toBeEnabled();
+  expect(items[2]).toBeEnabled();
+  expect(items[3]).toBeDisabled();
   await waitFor(() => expect(setSel).toBeCalledTimes(0));
 });
