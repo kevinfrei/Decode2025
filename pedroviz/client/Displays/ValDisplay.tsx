45ff98bff210c69a38f1d7022b7a2b7329d9b817
Kevin Frei <kevinfrei@hotmail.com>
2026-01-02 08:37:29 -0800

    File structure shuffling

---
+++ pedroviz/client/Displays/ValDisplay.tsx
@@ -0,0 +1,129 @@
+import { Input, InputProps, Text } from '@fluentui/react-components';
+import { useAtom, useAtomValue } from 'jotai';
+import { CSSProperties, ReactElement, useState } from 'react';
+
+import {
+  AnonymousValue,
+  isDoubleValue,
+  isIntValue,
+  isRadiansRef,
+  isRef,
+  ValueName,
+} from '../../server/types';
+import { MappedValuesAtom, ValueAtomFamily } from '../state/Atoms';
+
+export function EditableValueRef({
+  initial,
+  setRef,
+}: {
+  initial: ValueName;
+  setRef: (val: ValueName) => void;
+}): ReactElement {
+  const validRefs = useAtomValue(MappedValuesAtom);
+  const [curVal, setCurVal] = useState(initial);
+  const onChange: InputProps['onChange'] = (_, data) => {
+    if (validRefs.has(data.value as ValueName)) {
+      setRef(data.value as ValueName);
+    } else {
+      // TODO: Highlight the invalidity of this ref: Turn it red or something?
+    }
+    setCurVal(data.value as ValueName);
+  };
+  return (
+    <Input
+      type="text"
+      value={curVal}
+      onChange={onChange}
+      input={{ style: { textAlign: 'right' } }}
+    />
+  );
+}
+
+export function EditableValueExpr({
+  initial,
+  setVal,
+}: {
+  initial: number;
+  setVal: (v: AnonymousValue) => void;
+}): ReactElement {
+  const onChangeVal: InputProps['onChange'] = (_, data) => {
+    const newVal = Number.parseFloat(data.value);
+    if (!isNaN(newVal)) {
+      setVal(Number.isInteger(newVal) ? { int: newVal } : { double: newVal });
+    }
+  };
+  return (
+    <Input
+      type="number"
+      value={initial.toString()}
+      onChange={onChangeVal}
+      input={{ style: { textAlign: 'right' } }}
+    />
+  );
+}
+
+function getNumber(val: AnonymousValue): number {
+  return isDoubleValue(val) ? val.double : val.int;
+}
+
+export function NamedValueElem({ name }: { name: ValueName }): ReactElement {
+  const [item, setItem] = useAtom(ValueAtomFamily(name));
+  const type = isRadiansRef(item)
+    ? 'degrees'
+    : isIntValue(item)
+      ? 'int'
+      : 'double';
+  let editable: ReactElement;
+  if (isRadiansRef(item)) {
+    if (isRef(item.radians)) {
+      editable = (
+        <EditableValueRef
+          initial={item.radians}
+          setRef={(nm) => setItem({ radians: nm })}
+        />
+      );
+    } else {
+      editable = (
+        <EditableValueExpr
+          initial={getNumber(item.radians)}
+          setVal={(av) => setItem({ radians: av })}
+        />
+      );
+    }
+  } else if (isRef(item)) {
+    editable = <EditableValueRef initial={item} setRef={setItem} />;
+  } else {
+    editable = <EditableValueExpr initial={getNumber(item)} setVal={setItem} />;
+  }
+  return (
+    <>
+      <Text>{name}</Text>
+      {editable}
+      <Text>{type}</Text>
+    </>
+  );
+}
+
+export function NamedValueList(): ReactElement {
+  const items = useAtomValue(MappedValuesAtom);
+  const names = [...items.keys()];
+  const gridStyle: CSSProperties = {
+    display: 'grid',
+    columnGap: '10pt',
+    gridTemplateColumns: '1fr auto auto',
+    justifyItems: 'end',
+    justifySelf: 'start',
+    alignItems: 'center',
+  };
+
+  return (
+    <div style={gridStyle}>
+      <Text size={400}>Name {names.length}</Text>
+      <Text size={400}>Value</Text>
+      <Text size={400}>Units</Text>
+      {names.map((val) => (
+        <NamedValueElem key={val} name={val} />
+      ))}
+    </div>
+  );
+}
